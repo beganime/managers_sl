6ee37a6899f231cc0d08079d255dd94e5951f039
beganime <begenchyagmurow2008@gmail.com>
2026-02-19 21:07:39 +0500

    INIT

--- students_life/settings.py
+++ students_life/settings.py
@@ -78,8 +78,8 @@ if os.environ.get('DB_NAME'):
             'NAME': os.environ.get('DB_NAME'),
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': 'localhost',
-            'PORT': '5432',
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT':os.environ.get('DB_PORT')
         }
     }
 else:
