5dca687978922459b1a376e27b24b38ea825b5e0
beganime <begenchyagmurow2008@gmail.com>
2026-03-01 00:07:08 +0500

    Anaytics API Debug

--- analytics/serializers.py
+++ analytics/serializers.py
@@ -15,10 +15,10 @@ class ExpenseSerializer(serializers.ModelSerializer):
         read_only_fields = ('manager', 'amount_usd', 'updated_at')
 
 class DealSerializer(serializers.ModelSerializer):
-    # Вкладываем платежи внутрь сделки для удобства отображения в мобилке (только для чтения)
     payments = PaymentSerializer(many=True, read_only=True)
     
     class Meta:
         model = Deal
         fields = '__all__'
-        read_only_fields = ('manager', 'total_to_pay_usd', 'paid_amount_usd', 'payment_status', 'created_at', 'updated_at')
+        # ВАЖНО: Мы убрали 'total_to_pay_usd' из read_only, чтобы оно нормально сохранялось!
+        read_only_fields = ('manager', 'paid_amount_usd', 'payment_status', 'created_at', 'updated_at')
