3fbc1cd9c7e8541471676d65419649feb8b50597
beganime <begenchyagmurow2008@gmail.com>
2026-05-20 14:25:43 +0500

    Add CRM API urls

---
+++ apps/crm/urls.py
@@ -0,0 +1,25 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    ApplicationViewSet,
+    ClientActivityViewSet,
+    ClientFileViewSet,
+    ClientNoteViewSet,
+    ClientViewSet,
+    LeadSourceViewSet,
+    LeadViewSet,
+)
+
+router = DefaultRouter()
+router.register('lead-sources', LeadSourceViewSet, basename='crm-lead-source')
+router.register('leads', LeadViewSet, basename='crm-lead')
+router.register('clients', ClientViewSet, basename='crm-client')
+router.register('applications', ApplicationViewSet, basename='crm-application')
+router.register('activities', ClientActivityViewSet, basename='crm-activity')
+router.register('notes', ClientNoteViewSet, basename='crm-note')
+router.register('files', ClientFileViewSet, basename='crm-file')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
