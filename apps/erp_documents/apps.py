09aa4327391bd38e5614976560f1d457f90f46b3
beganime <begenchyagmurow2008@gmail.com>
2026-06-25 20:32:05 +0500

    Load document generation runtime fixes

--- apps/erp_documents/apps.py
+++ apps/erp_documents/apps.py
@@ -5,3 +5,8 @@ class ErpDocumentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.erp_documents'
     verbose_name = 'Документы'
+
+    def ready(self):
+        from .runtime_patches import apply_document_generation_patches
+
+        apply_document_generation_patches()
